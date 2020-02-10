8d52577dbd61ea90fe54d545021e9a8097a227ad
nonsense00027 <caneteneilfred@gmail.com>
2020-02-10 14:23:53 +0800

    2nd commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,14 +42,16 @@
                   <form method="POST" action="{{ route('login') }}">
                   @csrf
                     <div class="form-group">
+                    <label for="email" class="col-md-8 col-form-label text-md-left">{{ __('E-Mail Address') }}</label>
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                         @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                    </div>
-                    <div class="form-group">
+                    <!-- </div>
+                    <div class="form-group"> -->
+                    <label for="password" class="col-md-8 col-form-label text-md-left">{{ __('Password') }}</label>
                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                         @error('password')
                             <span class="invalid-feedback" role="alert">
